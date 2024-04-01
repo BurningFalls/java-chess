76c7a7ea60227b9cbf172399ada00b8264a24afb
BurningFalls <sherlocky31415@gmail.com>
2024-04-01 16:02:16 +0900

    refactor: 열 점수 계산 테스트 추가

--- src/test/java/chess/domain/board/LivePiecesInFileTest.java
+++ src/test/java/chess/domain/board/LivePiecesInFileTest.java
@@ -1,11 +1,16 @@
 package chess.domain.board;
 
+import chess.domain.piece.Knight;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Rook;
 import chess.domain.piece.WhitePawnFirstMove;
 import chess.domain.pieceinfo.PieceInfo;
+import chess.domain.pieceinfo.PieceScore;
 import chess.domain.pieceinfo.Position;
 import chess.domain.pieceinfo.Team;
+import chess.domain.strategy.KnightMoveStrategy;
+import chess.domain.strategy.RookMoveStrategy;
 import chess.domain.strategy.WhitePawnFirstMoveStrategy;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -20,6 +25,11 @@ public class LivePiecesInFileTest {
             new WhitePawnFirstMove(new PieceInfo(Position.of("a3"), Team.WHITE), WHITE_PAWN_STRATEGY);
     private static final Pawn THIRD_PAWN =
             new WhitePawnFirstMove(new PieceInfo(Position.of("a4"), Team.WHITE), WHITE_PAWN_STRATEGY);
+
+    private static final Rook WHITE_ROOK =
+            new Rook(new PieceInfo(Position.of("b1"), Team.WHITE), new RookMoveStrategy());
+    private static final Knight BLACK_KNIGHT =
+            new Knight(new PieceInfo(Position.of("b2"), Team.BLACK), new KnightMoveStrategy());
 
     @DisplayName("한 열에 폰이 한 개 있으면 합계는 1점이다.")
     @Test
@@ -53,6 +63,30 @@ public class LivePiecesInFileTest {
 
         double actualScoreSum = livePiecesInFile.calculatePieceScoreSum(Team.WHITE);
         double expectedScoreSum = 1.5;
+
+        Assertions.assertThat(actualScoreSum).isEqualTo(expectedScoreSum);
+    }
+
+    @DisplayName("한 열에 흰색 폰 하나와 검은색 나이트 하나가 있으면, 흰색 팀의 합계는 흰색 룩 하나의 점수와 같다.")
+    @Test
+    void calculateWhitePiecesScoreSumTest() {
+        List<Piece> livePieces = List.of(WHITE_ROOK, BLACK_KNIGHT);
+        LivePiecesInFile livePiecesInFile = new LivePiecesInFile(livePieces);
+
+        double actualScoreSum = livePiecesInFile.calculatePieceScoreSum(Team.WHITE);
+        double expectedScoreSum = PieceScore.ROOK_SCORE.get();
+
+        Assertions.assertThat(actualScoreSum).isEqualTo(expectedScoreSum);
+    }
+
+    @DisplayName("한 열에 흰색 폰 하나와 검은색 나이트 하나가 있으면, 검은색 팀의 합계는 검은색 나이트 하나의 점수와 같다.")
+    @Test
+    void calculateBlackPiecesScoreSumTest() {
+        List<Piece> livePieces = List.of(WHITE_ROOK, BLACK_KNIGHT);
+        LivePiecesInFile livePiecesInFile = new LivePiecesInFile(livePieces);
+
+        double actualScoreSum = livePiecesInFile.calculatePieceScoreSum(Team.BLACK);
+        double expectedScoreSum = PieceScore.KNIGHT_SCORE.get();
 
         Assertions.assertThat(actualScoreSum).isEqualTo(expectedScoreSum);
     }
